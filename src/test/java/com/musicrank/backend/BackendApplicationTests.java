29df78f1d7255a67d6ca374fa9ed83dd55354c27
Bruno Machado <brunomdr46@gmail.com>
2023-02-23 17:07:13 -0300

    Primeiro commit

---
+++ src/test/java/com/musicrank/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicrank.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
